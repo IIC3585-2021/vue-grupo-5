a0ee1ce0eb17098916065a37bb0486b14c5132e5
Josefina <jfernandez7@uc.cl>
2021-06-06 19:25:29 -0400

    first version disabled button

--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -1,15 +1,15 @@
 <template>
     <!-- card -->
   <div v-bind:style="{display: 'flex', flexDirection: 'column', backgroundColor: 'lightblue', width: 'auto', height: 'auto', padding: '5px', margin: '3px', borderRadius: '0.25rem'}">
-        <img id="image" v-bind:src="`${dog.image.url}`" v-bind:style="{margin: 'auto', display: 'flex', height: '100px', width: '100px', align: 'center'}">
+        <img id="image" v-bind:src="`${dog.image.url}`" v-bind:style="{display: 'flex', height: '200px', width: '200px', margin: '20px'}">
         <!-- info -->
-        <div v-bind:style="{display: 'flex', flexDirection: 'column', height: '160px', width: '200px'}">
-            <h3>Breed: {{dog.bred_for}}</h3>
-            <h3>Group: {{dog.breed_group}}</h3>
+        <div v-bind:style="{display: 'flex', flexDirection: 'column', height: '200px', width: '200px', margin: '20px'}">
+            <h3>Breed: {{dog.bred_for || "None"}}</h3>
+            <h3>Group: {{dog.breed_group || "None"}}</h3>
         </div>
         <button v-if="favorite == true" @click="removeDogFromFavorites(dog)">Delete from Favorites</button>
 
-        <button v-else @click="addDogToFavorites(dog)">Add to Favorites</button>
+        <button v-else id="add" onClick="this.disabled=true" @click="addDogToFavorites(dog)">Add to Favorites</button>
 
 
   </div>
