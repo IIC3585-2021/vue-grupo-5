528e3205f2b7e8b408c22143a0a276acf01ac75d
Josefina <jfernandez7@uc.cl>
2021-06-06 19:09:32 -0400

    images centered and language fix

--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -1,15 +1,15 @@
 <template>
     <!-- card -->
   <div v-bind:style="{display: 'flex', flexDirection: 'column', backgroundColor: 'lightblue', width: 'auto', height: 'auto', padding: '5px', margin: '3px', borderRadius: '0.25rem'}">
-        <img id="image" v-bind:src="`${dog.image.url}`" v-bind:style="{display: 'flex', height: '100px', width: '100px', align: 'center'}">
+        <img id="image" v-bind:src="`${dog.image.url}`" v-bind:style="{margin: 'auto', display: 'flex', height: '100px', width: '100px', align: 'center'}">
         <!-- info -->
         <div v-bind:style="{display: 'flex', flexDirection: 'column', height: '160px', width: '200px'}">
             <h3>Breed: {{dog.bred_for}}</h3>
             <h3>Group: {{dog.breed_group}}</h3>
         </div>
-        <button v-if="favorite == true" @click="removeDogFromFavorites(dog)">Eliminar</button>
+        <button v-if="favorite == true" @click="removeDogFromFavorites(dog)">Delete from Favorites</button>
 
-        <button v-else @click="addDogToFavorites(dog)">Agregar</button>
+        <button v-else @click="addDogToFavorites(dog)">Add to Favorites</button>
 
 
   </div>
